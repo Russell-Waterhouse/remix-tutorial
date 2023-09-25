ce5d817a1afe0b2f06fa17253dedcfb815ebaed5
Russell Waterhouse <russell@ruboss.com>
2023-09-25 10:33:05 -0700

    add global pending UI

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from "@remix-run/react";
 
 import { createEmptyContact, getContacts } from "./data";
@@ -32,6 +33,8 @@ export const action = async () => {
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
+  const { navigation } = useNavigation();
+
   return (
     <html lang="en">
       <head>
@@ -94,7 +97,13 @@ export default function App() {
           </nav>
         </div>
 
-        <div id="detail">
+        
+        <div 
+            className={
+                navigation.state === "loading" ? "loading" : ""
+            }
+            id="detail"
+        >
             <Outlet />
         </div>
         <ScrollRestoration />
